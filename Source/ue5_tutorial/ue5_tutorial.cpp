da97fe79ec7008f66dbc7df591172bf5a19b866c
pilot2254 <109202734+pilot2254@users.noreply.github.com>
2025-09-09 12:12:21 +0200

    Delete UE project

--- Source/ue5_tutorial/ue5_tutorial.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "ue5_tutorial.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ue5_tutorial, "ue5_tutorial" );
